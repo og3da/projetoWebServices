6954311915a1c5f650e83a1266db8210421c52ed
Raphael Ogeda <rapha488@gmail.com>
2023-09-27 21:06:59 -0300

    Project created

---
+++ src/main/java/com/educandoweb/projetoWebServices/ProjetoWebServicesApplication.java
@@ -0,0 +1,13 @@
+package com.educandoweb.projetoWebServices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoWebServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoWebServicesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/educandoweb/projetoWebServices/ProjetoWebServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.educandoweb.projetoWebServices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoWebServicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
