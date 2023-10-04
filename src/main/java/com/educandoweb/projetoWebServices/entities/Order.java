026dba4ba009550490102e1579a36cf42158ae75
Raphael Ogeda <71846924+og3da@users.noreply.github.com>
2023-10-04 10:00:42 -0300

    Order, Instant, ISO 8601 - part 1
    - entity "Order" created
    - associations between "Order" and "Customer"

---
+++ src/main/java/com/educandoweb/projetoWebServices/entities/Order.java
@@ -0,0 +1,80 @@
+package com.educandoweb.projetoWebServices.entities;
+
+import java.io.Serializable;
+import java.time.Instant;
+import java.util.Objects;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "tb_order")
+public class Order implements Serializable {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private Instant momement;
+	
+	// anotação p/ declarar chave estrangeira (muitos pedidos p/1 cliente)
+	@ManyToOne 
+	@JoinColumn(name = "client_id")
+	private User client;
+	
+	public Order() {}
+
+	public Order(Long id, Instant momement, User client) {
+		super();
+		this.id = id;
+		this.momement = momement;
+		this.client = client;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public Instant getMomement() {
+		return momement;
+	}
+
+	public void setMomement(Instant momement) {
+		this.momement = momement;
+	}
+
+	public User getClient() {
+		return client;
+	}
+
+	public void setClient(User client) {
+		this.client = client;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Order other = (Order) obj;
+		return Objects.equals(id, other.id);
+	}
+	
+	
+}
