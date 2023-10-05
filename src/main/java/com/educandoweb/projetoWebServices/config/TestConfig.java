3318cb521e12cd574a5b47bf695f35e6de86e2b9
Raphael Ogeda <71846924+og3da@users.noreply.github.com>
2023-10-05 14:10:28 -0300

    Category
    - implemented: order repository, service and resource
    - seeding H2 with order instances

--- src/main/java/com/educandoweb/projetoWebServices/config/TestConfig.java
+++ src/main/java/com/educandoweb/projetoWebServices/config/TestConfig.java
@@ -8,9 +8,11 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
+import com.educandoweb.projetoWebServices.entities.Category;
 import com.educandoweb.projetoWebServices.entities.Order;
 import com.educandoweb.projetoWebServices.entities.User;
 import com.educandoweb.projetoWebServices.entities.enums.OrderStatus;
+import com.educandoweb.projetoWebServices.repositories.CategoryRepository;
 import com.educandoweb.projetoWebServices.repositories.OrderRepository;
 import com.educandoweb.projetoWebServices.repositories.UserRepository;
 
@@ -28,11 +30,20 @@ public class TestConfig implements CommandLineRunner {
 	
 	@Autowired
 	private OrderRepository orderRepository;
+	
+	@Autowired
+	private CategoryRepository categoryRepository;
 
 	@Override
 	public void run(String... args) throws Exception {
-		
 		// id esta como null pois no banco sera automatico
+		
+		Category cat1 = new Category(null, "Electronics");
+		Category cat2 = new Category(null, "Books");
+		Category cat3 = new Category(null, "Computers");
+		
+		categoryRepository.saveAll(Arrays.asList(cat1, cat2, cat3));
+		
 		User u1 = new User(null, "Maria Brown", "maria@gmail.com", "988888888", "123456");
 		User u2 = new User(null, "Alex Green", "alex@gmail.com", "977777777", "123456");
 		
